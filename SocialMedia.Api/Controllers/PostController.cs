b1b9ac322a887fee5407832123b79eefb95575ec
David <david_horeb@hotmail.com>
2020-12-28 09:49:27 -0300

    Fix Api to Core 3.1

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Infrastructure.Repositories;
+using System.Collections.Generic;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetPosts()
+        {
+            IEnumerable<Core.Entities.Post> post = new PostRepository().GetPost();
+            return Ok(post);
+        }
+    }
+}
---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,24 @@
+﻿using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPost()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                PostId = x,
+                Description = $"Descripcion {x}",
+                Date = DateTime.Now,
+                UserId = x
+            });
+            return posts;
+        }
+
+    }
+}
